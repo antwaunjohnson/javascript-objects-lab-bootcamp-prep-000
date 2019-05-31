2072e3ca4556348db85b99ff937bdf8aa7e4dd69
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-31 01:45:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key]
   return newObj
 }
+
+function dstructivelyUpdateObjectWithKeyAndValue(object, key){
+  delete object[key]
+  return object
+}
